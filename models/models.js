bed0b32c4883a0e901bd152eaf32a2c64ac3ea4e
Rafael García Cabrera <rafa@iesvirgendelcarmen.com>
2015-07-14 21:00:58 +0200

    Quiz 7: sequelize.js y SQLite

---
+++ models/models.js
@@ -0,0 +1,31 @@
+// models.js construye la DB y el modelo importando (quiz.js)
+// sequelize.sync() construye la DB según define el modelo.
+
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null,
+			{dialect: "sqlite", storage: "quiz.sqlite"}
+		   );
+
+// Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().success(function() {
+  // success(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().success(function(count) {
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+      Quiz.create({ pregunta: 'Capital de Italia',
+		    respuesta: 'Roma'
+		 })
+      .success(function(){console.log('Base de datos inicializada')});
+     };
+   });
+});	
+
