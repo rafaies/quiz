ff7143717a71fafd9d5d4d3e9ad26d21a8877e95
Rafael García Cabrera <rafa@iesvirgendelcarmen.com>
2015-07-16 11:07:22 +0200

    Quiz 9: Múltiples preguntas

--- models/models.js
+++ models/models.js
@@ -35,14 +35,17 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-  // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function(count) {
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function(count) {
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta: 'Capital de Italia',
 		    respuesta: 'Roma'
+		 });
+      Quiz.create({ pregunta: 'Capital de Portugal',
+		    respuesta: 'Lisboa'
 		 })
-      .success(function(){console.log('Base de datos inicializada')});
+      .then(function(){console.log('Base de datos inicializada')});
      };
    });
 });	
--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,10 @@ router.get('/author', function(req, res) {
   res.render('author');
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+
+// Definición de rutas de /quizes 
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
